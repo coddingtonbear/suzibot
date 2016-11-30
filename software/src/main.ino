6364020bbcac88483070613a11c6c4e8ccdb126e
Adam Coddington <me@adamcoddington.net>
2016-11-29 23:54:10 -0800

    Switch to pin names rather than using artificially-assigned mightycore pin numbers.

--- software/src/main.ino
+++ software/src/main.ino
@@ -5,23 +5,24 @@
 #include "audio.h"
 #include "gps.h"
 #include "kline.h"
+#include "pins.h"
+#include "power.h"
 #include "sdcard.h"
 #include "wifi.h"
-#include "power.h"
 
-#define LED 15
-#define PIEZO 0
+#define LED PIN_PD7
+#define PIEZO PIN_PB0
 
-#define DISABLE_GPS 2
-#define ENABLE_LEVEL_CONVERTER 1
-#define ENABLE_3V3_REGULATOR A0
-#define RX_VOLTAGE A7
-#define TX_POWER_OFF 22
+#define DISABLE_GPS PIN_PB2
+#define ENABLE_LEVEL_CONVERTER PIN_PB1
+#define ENABLE_3V3_REGULATOR PIN_PA0
+#define RX_VOLTAGE PIN_PA7
+#define TX_POWER_OFF PIN_PC6
 
-#define CS_SD 4
-#define CS_KLINE 23
-#define CS_WIFI 3
-#define CS_GPS 3
+#define CS_SD PIN_PB4
+#define CS_KLINE PIN_PC7
+#define CS_WIFI PIN_PB3
+#define CS_GPS PIN_PB3
 
 EventManager event_manager;
 
---
+++ software/src/pins.h
@@ -0,0 +1,40 @@
+#ifndef MIGHTYCORE_PINS
+#define MIGHTYCORE_PINS 1
+
+#define PIN_PB0 0
+#define PIN_PB1 1
+#define PIN_PB2 2
+#define PIN_PB3 3
+#define PIN_PB4 4
+#define PIN_PB5 5
+#define PIN_PB6 6
+#define PIN_PB7 7
+
+#define PIN_PD0 8
+#define PIN_PD1 9
+#define PIN_PD2 10
+#define PIN_PD3 11
+#define PIN_PD4 12
+#define PIN_PD5 13
+#define PIN_PD6 14
+#define PIN_PD7 15
+
+#define PIN_PC0 16
+#define PIN_PC1 17
+#define PIN_PC2 18
+#define PIN_PC3 19
+#define PIN_PC4 20
+#define PIN_PC5 21
+#define PIN_PC6 22
+#define PIN_PC7 23
+
+#define PIN_PA0 A0
+#define PIN_PA1 A1
+#define PIN_PA2 A2
+#define PIN_PA3 A3
+#define PIN_PA4 A4
+#define PIN_PA5 A5
+#define PIN_PA6 A6
+#define PIN_PA7 A7
+
+#endif
